3d305c2038a176ed93b0f9faff617a4a30586373
Christoph1997 <99396989+Christoph1997@users.noreply.github.com>
2024-02-05 18:57:57 +0100

    Fix bug parallel

--- code/run_multi_sim.py
+++ code/run_multi_sim.py
@@ -6,11 +6,10 @@ import sys
 num_trials = 5
 
 prcs = []
-#max_prcs_count = 5
-max_concurrent_processes = 15
+max_prcs_count = 15
 
 learnrates = [0.01,0.1,0.2,0.4,0.8]
-"""
+
 try:
     for learnrate in learnrates:
         for frequency in range(1,7,2):
@@ -31,24 +30,3 @@ except KeyboardInterrupt:
     print("interrupted")
     for process in prcs:
         process.terminate()
-
-"""
-def run_trial(args):
-    idx, learnrate, frequency, amplitude = args
-    return Popen(['python3', 'run_reaching.py', str(idx), str(learnrate), str(frequency), str(amplitude)])
-
-try:
-    with ThreadPoolExecutor(max_concurrent_processes) as executor:
-        for learnrate in learnrates:
-            for frequency in range(1, 7, 2):
-                for amplitude in range(4, 20, 4):
-                    args_list = [(idx, learnrate, frequency, amplitude) for idx in range(num_trials)]
-                    processes = list(executor.map(run_trial, args_list))
-                    for process in processes:
-                        process.wait()
-
-except KeyboardInterrupt:
-    print("Interrupted")
-    for process in processes:
-        process.terminate()
-
